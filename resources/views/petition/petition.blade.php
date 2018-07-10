7e651d78213ee95a5fd80b9394c157674cd9a8e2
Diego Cerqueira <xdiegocerqueira@gmail.com>
2018-07-10 09:59:05 -0300

    visualization for petitions added

--- resources/views/petition/petition.blade.php
+++ resources/views/petition/petition.blade.php
@@ -1,12 +1,32 @@
 @extends('layouts.template')
 
 @section('title', 'Petitions')
-
-
+    
 @section('content')
-    @foreach ($petitions as $petition)
-        <li><a href="{{ action('PetitionController@showPetition', $petition->id) }}">{{ $petition->name }} </a></li>
-    @endforeach
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nome do Projeto</th>
+            <th scope="col">Proponente</th>
+            <th scope="col">Email</th>
+            <th scope="col">Enviado em</th>
+            <th scope="col">Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($petitions as $petition)
+            <tr>
+                <th>{{ $petition->id }}</th>
+                <td><a href="{{ action('PetitionController@showPetition', $petition->id) }}">{{ str_limit($petition->name, 30) }} </a>
+                <td>{{ $petition->sender_name }} </td>
+                <td>{{ $petition->sender_mail }} </td>
+                <td>{{ $petition->submitDate }} </td>
+                <td>{{ $petition->status_id }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 
     {{ $petitions->links() }}
 
