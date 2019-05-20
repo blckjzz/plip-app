0ddb3b5ed6dfc950cd16deaa02f53f0da49e4f8a
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-05-20 11:18:52 -0300

    fix and refactor on routes. removing middlewares from controllers to routes

--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,35 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::get('/', function () {
     return view('layouts.home');
 })->middleware('auth');
-Route::get('/petition', 'PetitionController@index');
-Route::get('/typeform/getAnwsers', 'TypeformController@getTypeformAnswers');
-Route::get('petition/details/{id}', 'PetitionController@showPetition');
-Route::get('petition/edit/{id}', 'PetitionController@edit');
-Route::get('/petition/assign', 'PetitionController@assign');
-Route::post('/petition/assign', 'PetitionController@saveAssign');
-Route::post('/petition/save', 'PetitionController@save');
-Route::get('/petition/sync', 'PetitionController@syncPlips');
-Route::get('/petition/in-analysis', 'PetitionController@showPetitionsInAnalysis');
-Route::get('/petition/new-petitions', 'PetitionController@showNewPetitions');
-Route::get('/trello/info', 'TrelloController@getTrelloBoardInfos');
-Route::get('/trello/create', 'TrelloController@createTrelloCard');
-Route::get('/trello/push', 'TrelloController@pushPlipToTrello');
-Route::resource('/voluntarios', 'VolunteerController');
-Route::resource('/analysis', 'AnalysisController');
-Route::get('/analysis/create/{petition_id}','AnalysisController@create');
+
+route::prefix('admin', ['middleware' => ['auth', 'isAdmin']])->group(function () {
+    Auth::routes();
+    Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+    Route::get('/petition', 'PetitionController@index');
+    Route::get('/typeform/getAnwsers', 'TypeformController@getTypeformAnswers');
+    Route::get('petition/details/{id}', 'PetitionController@showPetition');
+    Route::get('petition/edit/{id}', 'PetitionController@edit');
+    Route::get('/petition/assign', 'PetitionController@assign');
+    Route::post('/petition/assign', 'PetitionController@saveAssign');
+    Route::post('/petition/save', 'PetitionController@save');
+    Route::get('/petition/sync', 'PetitionController@syncPlips');
+    Route::get('/petition/in-analysis', 'PetitionController@showPetitionsInAnalysis');
+    Route::get('/petition/new-petitions', 'PetitionController@showNewPetitions');
+    Route::get('/trello/info', 'TrelloController@getTrelloBoardInfos');
+    Route::get('/trello/create', 'TrelloController@createTrelloCard');
+    Route::get('/trello/push', 'TrelloController@pushPlipToTrello');
+    Route::resource('/analysis', 'AnalysisController');
+    Route::get('/analysis/create/{petition_id}', 'AnalysisController@create');
+    Route::resource('/voluntarios', 'VolunteerController');
+});
+
+
+route::middleware(['auth', 'isVolunteer'])->prefix('voluntario')->group(function () {
+    Route::GET('/self-assign', 'VolunteerController@getSelfAssignView');
+    Route::POST('/self-assign', 'VolunteerController@saveSelfAssign');
+});
+
