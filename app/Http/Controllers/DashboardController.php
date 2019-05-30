e9fb05e8f7a76bbc03b6302a96e604339afdc2f2
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-05-30 17:48:54 -0300

    dashboard controller added

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Petition;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    public function getCardValues()
+    {
+        $analises = Auth::user()->volunteer->analises;
+
+        $analiseAprovada = DB::table('analyses')
+            ->join('petitions', 'petitions.id', '=', 'analyses.petition_id')
+            ->join('volunteers', 'volunteers.id', '=', 'analyses.volunteer_id')
+            ->where('volunteer_id', '=', Auth::user()->volunteer->id)
+            ->where('petitions.status_id', '=', 5)
+            ->get()->count();
+
+
+        $reprovadas = DB::table('analyses')
+            ->join('petitions', 'petitions.id', '=', 'analyses.petition_id')
+            ->join('volunteers', 'volunteers.id', '=', 'analyses.volunteer_id')
+            ->where('volunteer_id', '=', Auth::user()->volunteer->id)
+            ->where('petitions.status_id', '=', [4,5])
+            ->get()->count();
+
+
+        return view('layouts.home', compact('analises', 'reprovadas','analiseAprovada'));
+    }
+}
