06e536c9390b4405f04f15f5327b5d84530db921
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-05-30 17:44:38 -0300

    login and logout refactor

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,23 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('/login');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        if($user->role->id == 1){
+            return redirect('/admin');
+        }else{
+            return redirect('/voluntario');
+        }
     }
 }
