57c17443da1829a46b8fc03eb0037e6baa094457
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-06-11 10:54:21 -0300

    removing dashboard provider

--- config/app.php
+++ config/app.php
@@ -146,7 +146,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        \App\Providers\DashboardProvider::class,
 
         /*
          * Package Service Providers...
