d6dcc88c9bffaa3ed35e880d99f2edf68b0c9672
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-06-14 12:21:02 -0300

    improvements on layout and adding sobrenome to volunteers list

--- resources/views/petition/create-assignment.blade.php
+++ resources/views/petition/create-assignment.blade.php
@@ -3,53 +3,66 @@
 @section('title', 'Cadastrar analise' )
 @section('pageTitle', 'Cadastrar analise')
 @section('content')
-    <form id="cadastrar" method="POST" action="{{action('PetitionController@saveAssign')}}">
-        {{ csrf_field() }}
-        <div class="row">
-            <div class="col-md-12">
-                <div class="btn-group">
+    <div class="row p-1">
+        <div class="col-sm-10">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                    <div class="col-md-12">
+                        <form id="cadastrar" method="POST" action="{{action('PetitionController@saveAssign')}}">
+                            {{ csrf_field() }}
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="btn-group">
 
-                    <a href="{{ URL::previous() }}">
-                        <button type="button" class="btn btn-primary">Voltar</button>
-                    </a>
-                </div>
-                <div class="btn-group">
+                                        <a href="{{ URL::previous() }}">
+                                            <button type="button" class="btn btn-primary">Voltar</button>
+                                        </a>
+                                    </div>
+                                    <div class="btn-group">
 
 
-                    <button type="Submit" value="Editar" class="btn btn-success">Cadastrar</button>
+                                        <button type="Submit" value="Editar" class="btn btn-success">Cadastrar</button>
 
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="">Projeto</label>
+                                        <select name="project_id" class="form-control">
+                                            <option selected="true" disabled="disabled">Selecione um projeto</option>
+                                            @foreach($petitions as $petition)
+                                                <option value="{{$petition->id}}">
+                                                    {{ $petition->id .' - '.$petition->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="">Voluntário</label>
+                                        <select name="volunteer_id" class="form-control">
+                                            <option selected="true" disabled="disabled">Selecione um Voluntário</option>
+                                            @foreach($volunteers as $volunteer)
+                                                <option value="{{$volunteer->id}}">
+                                                    {{ $volunteer->user->name .' '. $volunteer->user->sobrenome}}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label for="">Projeto</label>
-                    <select name="project_id" class="form-control">
-                        <option selected="true" disabled="disabled">Selecione um projeto</option>
-                        @foreach($petitions as $petition)
-                            <option value="{{$petition->id}}">
-                                {{ $petition->name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Voluntário</label>
-                    <select name="volunteer_id" class="form-control">
-                        <option selected="true" disabled="disabled">Selecione um Voluntário</option>
-                        @foreach($volunteers as $volunteer)
-                            <option value="{{$volunteer->id}}">
-                                {{ $volunteer->user->name}}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-        </div>
-    </form>
 @endsection
 @section('scripts')
     <script>
